93f2a3cfd8539f10c5c9207395e68d69a331e2c5
dtasos <dimitriou.tas@gmail.com>
2020-08-14 07:28:00 +0300

    page 70

--- App.js
+++ App.js
@@ -1,46 +1,95 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Platform, TextInput, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Text, View, Platform, TextInput, KeyboardAvoidingView, ImageBackground } from 'react-native';
+
+import getImageForWeather from './utils/getImageForWeather'
 
 import SearchInput from './components/SearchInput';
 
-export default function App() {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      location: ''
+    }
+  }
+
+  componentDidMount() {
+    this.handleUpdateLocation('San Francisco')
+  }
+
+  handleUpdateLocation = city => {
+    this.setState({
+      location: city,
+    })
+  }
+
+  render() {
+    const {location} = this.state
+
   return (
-    <KeyboardAvoidingView 
+    <KeyboardAvoidingView
       style={styles.container}
       behavior="padding">
-      <Text style={[styles.largeText, styles.textStyle]}>
-        San Francisco
-      </Text>
-      <Text style={[styles.smallText, styles.textStyle]}>
-        Light Cloud
-      </Text>
-      <Text style={[styles.largeText, styles.textStyle]}>
-        24*
-      </Text>
-      <SearchInput
-       placeholder="Search any city"/>
 
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={getImageForWeather('Clear')}
+        style={styles.imageContainer}
+        imageStyle={styles.image}
+      >
+        <View style={styles.detailsContainer}>
+
+          <Text style={[styles.largeText, styles.textStyle]}>
+            {location}
+        </Text>
+          <Text style={[styles.smallText, styles.textStyle]}>
+            Light Cloud
+        </Text>
+          <Text style={[styles.largeText, styles.textStyle]}>
+            24*
+        </Text>
+          <SearchInput
+            placeholder="Search any city"
+            onSubmit={this.handleUpdateLocation}
+          />
+        </View>
+
+        <StatusBar style="auto" />
+      </ImageBackground>
     </KeyboardAvoidingView>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#34495E',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image:{
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
   },
   textStyle: {
     textAlign: 'center',
     fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
   },
   largeText: {
-    fontSize:44, 
+    fontSize: 44,
   },
   smallText: {
     fontSize: 18,
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
   },
 });
